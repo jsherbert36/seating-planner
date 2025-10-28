35df0d82cc5a727e5e1501134b8ac42c326a9f55
Jacob Herbert <54801612+jsherbert36@users.noreply.github.com>
2025-10-28 19:42:01 +0000

    test commit

---
+++ backend/src/test/java/com/ethos/seating_planner/SeatingPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ethos.seating_planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeatingPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
